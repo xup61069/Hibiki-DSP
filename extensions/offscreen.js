48ab34f0aeecf0a43979f3be494b68cf00fe693f
Kadid <xup61069@gmail.com>
2026-08-26 07:26:52 +0800

    extension: fix undefined retryInSec in offscreen reportState (#1639)
    Closes #1634.

--- extensions/offscreen.js
+++ extensions/offscreen.js
@@ -36,7 +36,7 @@ function reportState() {
     bridgeConnected,
     droppedPackets,
     bridgeReconnectState: bridgeReconnectState(),
-    bridgeRetryInSec: retryInSec,
+    bridgeRetryInSec: bridgeRetryInSec(),
   });
 }
 
